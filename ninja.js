9b61753ec1c1a6c65c8ca127bf33e94130d54412
mfcg15 <fcardenas1504@gmail.com>
2022-04-25 16:34:47 -0500

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -1,11 +1,19 @@
 class Ninja
 {
-    constructor(nombre,salud)
+    /*constructor(nombre,salud)
     {
         this.nombre = nombre;
         this.salud = salud;
         this.velocidad = 3;
         this.fuerza = 3;
+    }*/
+
+    constructor(nombre,salud, velocidad, fuerza)
+    {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.velocidad = velocidad;
+        this.fuerza = fuerza;
     }
 
     sayName ()
@@ -24,7 +32,25 @@ class Ninja
     }
 }
 
-const ninja1 = new Ninja("Hyabusa",10);
+class Sensei extends Ninja 
+{
+    constructor(nombre) 
+    {
+        super(nombre, 200,10,10);
+        this.sabiduria = 10;
+    }
+
+    speakWisdom()
+    {
+        super.drinkSake();
+    }
+}
+
+/*const ninja1 = new Ninja("Hyabusa",10);
 ninja1.sayName();
 ninja1.drinkSake();
-ninja1.showStats();
+ninja1.showStats();*/
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
